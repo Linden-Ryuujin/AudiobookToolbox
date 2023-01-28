85e5a54fe408bdeb3d29832e2bed48958f6c4a34
Linden Ryuujin <Linden.Ryuujin@gmail.com>
2023-01-28 02:11:38 +0000

    Add support for dumping chapter files. Renamed the existing chapter files to match the export format used by mp4chaps.

--- chapters.sh
+++ chapters.sh
@@ -3,7 +3,8 @@ function PrintHelp {
     echo "chapters.sh ACTION [ACTION PARAMTERS]"
     echo
     echo Actions:
-    echo   "    -a --add [chapters.txt] [audiobook.mb4]         Add the chapters in [chapters.txt] to the audiobook in quicktime format."
+    echo   "    -a, --add       [chapters.txt] [audiobook.m4b]  Add the chapters in [chapters.txt] to the audiobook in quicktime format."
+    echo   "    -d, --dump      [audiobook.m4B]                 Dump the chapters from the audiobook to [audiobook.chapters.txt]"
 }
 
 function Add {
@@ -18,8 +19,16 @@ function Add {
     mp4chaps --convert --chapter-qt "$audiobook"
 }
 
+function Dump {
+    audiobook="$1"
+
+    echo "Dumping chapters from audiobook: \"$audiobook\""
+    mp4chaps --export "$audiobook"
+}
+
 case "$1" in
     -a|--add) shift; Add "$@" ;;
+    -d|--dump) shift; Dump "$@" ;;
     -h|--help) PrintHelp ;;
     *) echo "Unknown action: '$1'"
 esac
